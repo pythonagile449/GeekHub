f4bbe42eebcd6041c4fd5d9a43d627a539a7bca3
tyrrannt <shakirov.vitaliy@gmail.com>
2021-06-23 19:28:22 +0300

    Changed login templates

---
+++ commentsapp/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.hashers import make_password
+from usersapp.models import GeekHubUser
+# Create your models here.
+
+
+class Comment(models.Model):
+    class Meta:
+        abstract = True
+
+    parent_comment = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+    description = models.TextField(verbose_name='Текст комментария', blank=True)
+    author = models.ForeignKey(GeekHubUser, verbose_name='Автор комментария', on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(verbose_name='Время создания', auto_now_add=True)
+    hash_view = models.CharField(max_length=256, blank=True)
+
+    def __str__(self):
+        if self.parent_comment:
+            return f'{self.parent_comment}/{self.pk}'
+        else:
+            return str(self.pk)
+
+    def save(self, **kwargs):
+        some_salt = 'some_salt'
+        self.hash_view = make_password(self.hash_view, some_salt)
+        super().save(**kwargs)
+
+
+class CommentsBranch(Comment):
+    def __init__(self, *args, **kwargs):
+        super(CommentsBranch, self).__init__(*args, **kwargs)
