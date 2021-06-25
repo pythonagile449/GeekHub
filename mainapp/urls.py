d49deb30bc1d4922b2d4ebe4aa1194ae12e5af83
dan <gl.8700@gmail.com>
2021-06-26 01:01:11 +0300

    Added create article functional

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,5 +5,6 @@ app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.mainpage, name='index'),
-    path('hub/<str:hub_name>', mainapp.hubs, name='hubs')
+    path('hub/<str:hub_name>', mainapp.hubs, name='hubs'),
+    path('create-article/', mainapp.CreateArticle.as_view(), name='create_article'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from mainapp.forms import ArticleForm
+
 from mainapp.models import Hub, Article
 
 # Create your views here.
 from django.shortcuts import render
+
+from usersapp.models import GeekHubUser
 
 
 def mainpage(request):
@@ -24,3 +30,19 @@ def hubs(request, hub_name):
     }
 
     return render(request, 'mainapp/hub.html', context)
+
+
+class CreateArticle(CreateView):
+    """
+    Создание статьи
+    """
+    model = Article
+    form_class = ArticleForm
+    success_url = reverse_lazy('mainapp:index')
+
+    def form_valid(self, form):
+        """
+        Устанавливаем инстанс автора для FK модели Article
+        """
+        form.instance.author = self.request.user
+        return super(CreateArticle, self).form_valid(form)
