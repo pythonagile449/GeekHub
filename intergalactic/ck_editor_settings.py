606c64e0c140e16ffb0a4727a0b4544936ae0efb
dan <gl.8700@gmail.com>
2021-06-30 13:52:57 +0300

    create article template with styles finished

--- intergalactic/ck_editor_settings.py
+++ intergalactic/ck_editor_settings.py
@@ -9,7 +9,9 @@ CKEDITOR_CONFIGS = {
     'default': {
         'skin': 'moono',
         'toolbar': 'full',
+        'width': 'auto',
     },
+
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
--- mainapp/forms.py
+++ mainapp/forms.py
@@ -6,10 +6,11 @@ from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 
 class ArticleCkForm(forms.ModelForm):
-    hub = forms.ModelChoiceField(widget=forms.Select, empty_label='----', queryset=Hub.objects.all(), required=True)
-    title = forms.CharField(label='Название статьи', widget=forms.TextInput(attrs={'class': 'form-control'}),
+    hub = forms.ModelChoiceField(label='Название хаба', widget=forms.Select(attrs={'class': 'select-3'}),
+                                 empty_label='-- Выберите Хаб --', queryset=Hub.objects.all(), required=True)
+    title = forms.CharField(label='Название статьи', widget=forms.TextInput(attrs={'class': 'editor-placeholder'}),
                             max_length=256, required=True)
-    contents = forms.CharField(widget=CKEditorUploadingWidget())
+    contents = forms.CharField(widget=CKEditorUploadingWidget(attrs={'style': 'width: auto'}))
     short_description = forms.CharField(label='Краткое описание',
                                         widget=forms.TextInput(attrs={'class': 'form-control'}),
                                         max_length=256, required=True)
