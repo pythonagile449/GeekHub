3d1b3d37663ed9239a8c8973307a36cd7991c6b5
dan <gl.8700@gmail.com>
2021-07-04 10:06:08 +0300

    Fixed error with context processors for anonymous user

--- usersapp/context_processors/user_context_processors.py
+++ usersapp/context_processors/user_context_processors.py
@@ -1,10 +1,15 @@
+from django.contrib.auth.models import AnonymousUser
+
 from mainapp.models import Article
 
 
 def usersapp_context(request):
     user = request.user
-    user_drafts_count = Article.objects.filter(author=user, is_draft=True, is_deleted=False).count()
-    user_published_articles_count = Article.objects.filter(author=user, is_published=True, is_deleted=False).count()
+    user_drafts_count = 0
+    user_published_articles_count = 0
+    if not isinstance(user, AnonymousUser):
+        user_drafts_count = Article.objects.filter(author=user, is_draft=True, is_deleted=False).count()
+        user_published_articles_count = Article.objects.filter(author=user, is_published=True, is_deleted=False).count()
     return {
         'user_drafts_count': user_drafts_count,
         'user_published_articles_count': user_published_articles_count
