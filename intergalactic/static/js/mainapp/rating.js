082b3bc70b62fd580e272a847a1afb2e50a33c78
tyrrannt <62670631+tyrrannt@users.noreply.github.com>
2021-07-07 21:38:16 +0300

    9

--- intergalactic/static/js/mainapp/rating.js
+++ intergalactic/static/js/mainapp/rating.js
@@ -26,8 +26,8 @@ function like()
         url : "rating" + "/" + type + "/" + action + "/",
         type : 'POST',
         data : { 'obj' : pk },
-
         success : function (json) {
+            console.log(json)
             $('.positive').html(json.positive)
             $('.negative').html(json.negative)
         }
@@ -57,6 +57,50 @@ function dislike()
 
     return false;
 }
+
+function likeComment()
+{
+    var likeComment = $(this);
+    var type = likeComment.data('type');
+    var pk = likeComment.data('id');
+    var action = likeComment.data('action');
+    var dislike = likeComment.next();
+    console.log(type)
+    $.ajax({
+        url : "rating" + "/" + type + "/" + action + "/",
+        type : 'POST',
+        data : { 'obj' : pk },
+        success : function (json) {
+            console.log(json)
+            $('.positiveComment').html(json.positive)
+            $('.negativeComment').html(json.negative)
+        }
+    });
+    return false;
+}
+
+function dislikeComment()
+{
+    var dislikeComment = $(this);
+    var type = dislikeComment.data('type');
+    var pk = dislikeComment.data('id');
+    var action = dislikeComment.data('action');
+    var like = dislikeComment.prev();
+
+    $.ajax({
+        url : "rating" + "/" + type + "/" + action + "/",
+        type : 'POST',
+        data : { 'obj' : pk },
+        success : function (json) {
+            console.log(json)
+            $('.positiveComment').html(json.positive)
+            $('.negativeComment').html(json.negative)
+        }
+    });
+
+    return false;
+}
+
 // Подключение обработчиков
 $(function() {
     $.ajaxSetup({
@@ -64,4 +108,6 @@ $(function() {
     });
     $('[data-action="like"]').click(like);
     $('[data-action="dislike"]').click(dislike);
+    $('[data-action="likeComment"]').click(likeComment);
+    $('[data-action="dislikeComment"]').click(dislikeComment);
 });
--- ratingsapp/urls.py
+++ ratingsapp/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 from ratingsapp.models import RatingCount
 from mainapp.models import Article
+from commentsapp.models import CommentsBranch
 
 app_name = 'ajax'
 urlpatterns = [
@@ -13,10 +14,10 @@ urlpatterns = [
     path('article/dislike/',
          login_required(views.RatingsView.as_view(model=Article, vote_type=RatingCount.NEGATIVE)),
          name='article_dislike'),
-    # path('comment/like/',
-    #      login_required(views.RatingsView.as_view(model=Comment, vote_type=RatingCount.POSITIVE)),
-    #      name='article_like'),
-    # path('article/dislike/',
-    #      login_required(views.RatingsView.as_view(model=Article, vote_type=RatingCount.NEGATIVE)),
-    #      name='article_dislike'),
+    path('comment/like/',
+         login_required(views.RatingsView.as_view(model=CommentsBranch, vote_type=RatingCount.POSITIVE)),
+         name='comment_like'),
+    path('comment/dislike/',
+         login_required(views.RatingsView.as_view(model=CommentsBranch, vote_type=RatingCount.NEGATIVE)),
+         name='comment_dislike'),
 ]
