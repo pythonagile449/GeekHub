194b917acc3e72889bb22d48afda3d147af677c5
tyrrannt <62670631+tyrrannt@users.noreply.github.com>
2021-07-11 14:34:57 +0300

    Implemented a ban on cheating your rating

--- static/js/mainapp/rating.js
+++ static/js/mainapp/rating.js
@@ -27,10 +27,11 @@ function like() {
         url: "rating" + "/" + type + "/" + action + "/",
         type: 'POST',
         data: {'obj': pk},
-
         success: function (json) {
-            like.find("[data-count='positive']").text(json.positive);
-            dislike.find("[data-count='negative']").text(json.negative);
+            console.log(json)
+            $('.positive').html(json.positive)
+            $('.negative').html(json.negative)
+            $('.modal-content').html(json.access_denied)
         }
     });
     return false;
@@ -49,19 +50,82 @@ function dislike() {
         data: {'obj': pk},
 
         success: function (json) {
-            dislike.find("[data-count='negative']").text(json.negative);
-            like.find("[data-count='positive']").text(json.positive);
+            console.log(json)
+            $('.positive').html(json.positive)
+            $('.negative').html(json.negative)
+            $("#modal-articles .modal-content").html(json.access_denied);
         }
     });
 
     return false;
 }
 
-// Подключение обработчиков
+
+// Set handlers
 $(function () {
     $.ajaxSetup({
         headers: {"X-CSRFToken": getCookie("csrftoken")}
     });
-    $('[data-action="like"]').click(like);
-    $('[data-action="dislike"]').click(dislike);
+    $('.like-article').click(like);
+    $('.dislike-article').click(dislike);
 });
+
+
+class CommentsRating {
+    constructor() {
+        this.likeButtons = document.querySelectorAll('.like-comment-btn');
+        this.dislikeButtons = document.querySelectorAll('.dislike-comment-btn');
+        this.setHandlers();
+    }
+
+    setHandlers() {
+        this.likeButtons.forEach(button => {
+            button.addEventListener('click', evt => {
+                let requestParams = this.getAjaxParams(evt);
+                this.sendCommentRatingAjax(requestParams, evt);
+            })
+        })
+
+        this.dislikeButtons.forEach(button => {
+            button.addEventListener('click', evt => {
+                let requestParams = this.getAjaxParams(evt);
+                this.sendCommentRatingAjax(requestParams, evt);
+            })
+        })
+    }
+
+    getAjaxParams(evt) {
+        let targetSet = evt.target.parentNode; // get parentNode (<li>) to catch dataset params
+        let requestParams = {
+            type: targetSet.dataset.type,
+            action: targetSet.dataset.action,
+            formData: new FormData(document.querySelector('.comment-form')), // to set csrf token
+        };
+        requestParams.formData.append('obj_id', targetSet.dataset.id);
+
+        return requestParams;
+    }
+
+    sendCommentRatingAjax(requestParams, evt) {
+        $.ajax({
+            url: `rating/${requestParams.type}/${requestParams.action}/`,
+            type: 'POST',
+            data: requestParams.formData,
+            cache: false,
+            processData: false,
+            contentType: false,
+            enctype: 'multipart/form-data',
+            success: json => {
+                console.log(json);
+                evt.target.parentNode.parentNode.querySelector('.positiveComment').innerHTML = json.positive;
+                evt.target.parentNode.parentNode.querySelector('.negativeComment').innerHTML = json.negative;
+            },
+            error: d => {
+                console.log('err');
+                console.log(d);
+            }
+        })
+    }
+}
+
+export {CommentsRating};
