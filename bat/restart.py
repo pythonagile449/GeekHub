a8badcf8feb3ec84109eea89c3f4daa7ab91bca9
Daniil_Gayvoronsky <qwer.82@mail.ru>
2021-07-14 15:52:40 +0300

    transfer .bat files to the bat folder. To restart >bat\restart.py (windows)

---
+++ bat/restart.py
@@ -0,0 +1,73 @@
+import os
+import glob
+import shutil
+import re
+import subprocess
+
+
+
+def main():
+
+    # initializing the count
+    deleted_files_count = 0
+
+    # specify the path
+    path = os.getcwd()
+
+    # checking whether the file is present in path or not
+    if os.path.exists(path):
+
+        # iterating over each and every folder and file in the path
+        # for base.sqlite3, root, files in glob(path):
+
+        files = glob.glob(path + '/**/*.py', recursive=True)
+        base = glob.glob(path + '/**/*.sqlite3', recursive=True)
+        for file in base:
+            remove_file(file)
+            deleted_files_count += 1 # incrementing count
+
+
+                # checking the current directory files
+        for file in files:
+            # comparing the days
+            if re.findall(r'\d{4}_', file):
+                if not re.findall(r'venv', file):
+                    # invoking the remove_file function
+                    remove_file(file)
+                    deleted_files_count += 1 # incrementing count
+    print(f"Total files deleted: {deleted_files_count}")
+
+    os.startfile(path+'\\bat\mmigr.bat')
+    input('Нажмите Enter')
+    os.startfile(path+'\\bat\migr.bat')
+    input('Нажмите Enter')
+    os.startfile(path+'\\bat\\user.bat')
+    input('Нажмите Enter')
+    os.startfile(path+'\\bat\mmigr.bat')
+    input('Нажмите Enter')
+    os.startfile(path+'\\bat\migr.bat')
+    input('Нажмите Enter')
+    os.startfile(path+'\\bat\hubs.bat')
+    input('Нажмите Enter')
+    os.startfile(path+'\\bat\\article.bat')
+    input('Нажмите Enter')
+    os.startfile(path+'\\bat\draft.bat')
+    input('Нажмите Enter')
+    os.startfile(path+'\\bat\moderat.bat')
+    input('Нажмите Enter')
+    os.startfile(path+'\\bat\public.bat')
+    input('Нажмите Enter')
+
+def remove_file(path):
+    # removing the file
+    if not os.remove(path):
+        # success message
+        print(f"{path} is removed successfully")
+    else:
+        # failure message
+        print(f"Unable to delete the {path}")
+
+
+
+if __name__ == '__main__':
+    main()
