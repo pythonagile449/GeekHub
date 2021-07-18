ddd139f8ecc2e04330bc5ada4dd4cf747fb4fc92
dan <gl.8700@gmail.com>
2021-07-18 22:43:42 +0300

    working at search by target_types in progress

--- search/views.py
+++ search/views.py
@@ -3,33 +3,48 @@ from django.contrib.postgres.search import SearchQuery, SearchVector, SearchRank
 from django.db.models import Q
 from django.views.generic import ListView
 
-from mainapp.models import Article
+from commentsapp.models import CommentsBranch
+from mainapp.models import Article, Hub
+from usersapp.models import GeekHubUser
+
+TARGET_TYPES = {
+    'posts': Article,
+    'hubs': Hub,
+    'users': GeekHubUser,
+    'comments': CommentsBranch,
+}
 
 
 class Search(ListView):
-    template_name = 'mainapp/index.html'
-    context_object_name = 'articles'
+    template_name = 'search/search_page.html'
+    context_object_name = 'targets'
     paginate_by = 5
 
     def get_queryset(self):
         queryset = Article.objects.none()
-        if self.request.GET.get('q'):
+        print(self.request.GET)
+        query_string = self.request.GET.get('q')
+        target_type = self.request.GET.get('target_type')
+        if query_string and target_type in TARGET_TYPES.keys():
             if 'sqlite' in settings.DATABASES['default']['ENGINE']:
-                queryset = self.search_in_sqlite()
+                queryset = Search.search_in_sqlite(query_string, target_type)
             if 'postgre' in settings.DATABASES['default']['ENGINE']:
-                queryset = self.search_in_postgres()
+                queryset = Search.search_in_postgres(query_string, target_type)
         return queryset
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(Search, self).get_context_data()
         context['title'] = 'Поиск'
-        context['search'] = True
         context['q'] = self.request.GET.get('q')
+        context['target_type'] = self.request.GET.get('target_type')
         return context
 
-    def search_in_sqlite(self):
-        queryset = not Article.objects.filter(is_deleted=False)
-        for word in self.request.GET.get('q').split():
+    @staticmethod
+    def search_in_sqlite(query_string, target_type):
+        """ Search in development DB. """
+        # TODO доделать посик по тестовой БД
+        queryset = Article.objects.filter(is_deleted=False)
+        for word in query_string.split():
             q_list = Q()
             q_list |= Q(title__icontains=word)
             q_list |= Q(contents_ck__icontains=word)
@@ -37,11 +52,30 @@ class Search(ListView):
             queryset = Article.objects.filter(q_list)
         return queryset
 
-    def search_in_postgres(self):
+    @staticmethod
+    def search_in_postgres(query_string, target_type):
+        """ Search in postgres DB with vector and rank. Used for deploy settings."""
         search_query = SearchQuery('')
-        for word in self.request.GET.get('q').split():
+        for word in query_string.split():
             search_query |= SearchQuery(word)
-        search_vector = SearchVector('title', 'contents_ck', 'contents_md')
+        model = TARGET_TYPES[target_type]
+        search_vector = Search.set_search_vector(model)
         search_rank = SearchRank(search_vector, search_query)
-        queryset = Article.objects.annotate(rank=search_rank).order_by('-rank')
+        queryset = model.objects.annotate(rank=search_rank).order_by('-rank')
+        # todo деделать поиск по частичным совпадениям в словах
+        # todo возможно добавить в настройки пользователя поиск по неточным совпадениям
+        # queryset = model.objects.annotate(search=search_vector).filter(search=search_query)
+
         return queryset
+
+    @staticmethod
+    def set_search_vector(model):
+        """ Set search fields depend model. """
+        if issubclass(model, Article):
+            return SearchVector('title', 'contents_ck', 'contents_md')
+        if issubclass(model, Hub):
+            return SearchVector('name')
+        if issubclass(model, GeekHubUser):
+            return SearchVector('username')
+        if issubclass(model, CommentsBranch):
+            return SearchVector('description')
