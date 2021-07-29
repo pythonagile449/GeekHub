4384beceb60061e7db864cdf0b1d3a16c2a65b68
dan <gl.8700@gmail.com>
2021-07-29 16:29:57 +0300

    Added user like-dislike for article visability

--- usersapp/urls.py
+++ usersapp/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('logout/', users_view.UserLogoutView.as_view(), name='logout'),
     path('registration/', users_view.RegistrationView.as_view(), name='registration'),
     path('user/<uuid:pk>/', users_view.UserAccountView.as_view(), name='user_detail'),
-    path('edit/<uuid:pk>', users_view.UserAccountEdit.as_view(), name='modify'),
-    path('delete/<uuid:pk>', users_view.UserAccountDeleteView.as_view(), name='delete'),
+    path('edit/<uuid:pk>/', users_view.UserAccountEdit.as_view(), name='modify'),
+    path('delete/<uuid:pk>/', users_view.UserAccountDeleteView.as_view(), name='delete'),
     path('verify/<email>/<activate_key>/', users_view.verify, name='verify'),
     path('verify/', users_view.verification, name='verification'),
 ]
